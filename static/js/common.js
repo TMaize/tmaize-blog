3057a65869aae518779f9f5a5b08b787cc83411c
TMaize <1772314831@qq.com>
2017-06-16 21:11:05 +0800

    添加动画

--- static/js/common.js
+++ static/js/common.js
@@ -8,3 +8,37 @@ function getScrollTop() {
 	}
 	return scrollTop;
 }
+
+function showSheHuiZhuYi() {
+	function f(ev) {
+		var clickTips = ["富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善"];
+		var span = document.createElement('span');
+		span.innerText = clickTips[parseInt(Math.random()*clickTips.length)];
+		span.setAttribute("style","z-index:999;position:absolute;left:"+ev.pageX+"px;top:"+(ev.pageY-20)+"px;animation-duration:1s;" +
+				"animation-fill-mode:both;animation-name:fadeOutUp;");
+		document.body.appendChild(span);
+		//WebKit
+		if(typeof document.body.style.WebkitAnimation!="undefined"){
+			span.addEventListener("webkitAnimationEnd",function () {
+				document.body.removeChild(span);
+			});
+		}else{
+			span.addEventListener("animationend",function () {
+				document.body.removeChild(span);
+			});
+		}
+	}
+	if (document.all){
+		window.attachEvent("onclick",f);
+	}
+	else{
+		window.addEventListener("click",f,false);
+	}
+}
+
+if (document.all){
+	window.attachEvent("onload",showSheHuiZhuYi);
+}
+else{
+	window.addEventListener("load",showSheHuiZhuYi,false);
+}
