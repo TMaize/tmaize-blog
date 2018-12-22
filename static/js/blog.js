63c2663b3a4031d77de2e5880822fd8219f9a6a2
changboa66 <135295529@qq.com>
2018-12-22 17:39:19 +0800

    去掉console的打印

--- static/js/blog.js
+++ static/js/blog.js
@@ -164,7 +164,7 @@ blog.addLoadEvent(function () {
 // });
 
 //主题标识
-blog.addLoadEvent(function () {
-    console.info("Author Blog %chttp://blog.tmaize.net", "color: #FF0000;");
-    console.info("Github Site %chttps://github.com/TMaize/tmaize-blog", "color: #FF0000;");
-});
+// blog.addLoadEvent(function () {
+    // console.info("Author Blog %chttp://blog.tmaize.net", "color: #FF0000;");
+    // console.info("Github Site %chttps://github.com/TMaize/tmaize-blog", "color: #FF0000;");
+// });
