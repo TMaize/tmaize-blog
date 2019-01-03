519f4a8e2479a6da70a75f49e5e9b230f1c9c21a
tym <1772314831@qq.com>
2019-01-03 23:46:13 +0800

    page

--- static/js/blog.js
+++ static/js/blog.js
@@ -1,102 +1,111 @@
-
 /**
  * 工具，允许多次onload不被覆盖
- * @param {方法} func 
+ * @param {方法} func
  */
-blog.addLoadEvent = function (func) {
-    var oldonload = window.onload;
-    if (typeof window.onload != 'function') {
-        window.onload = func;
-    } else {
-        window.onload = function () {
-            oldonload();
-            func();
-        }
+blog.addLoadEvent = function(func) {
+  var oldonload = window.onload
+  if (typeof window.onload != 'function') {
+    window.onload = func
+  } else {
+    window.onload = function() {
+      oldonload()
+      func()
     }
+  }
 }
 
 /**
  * 工具，兼容的方式添加事件
- * @param {单个DOM节点} dom 
- * @param {事件名} eventName 
- * @param {事件方法} func 
- * @param {是否捕获} useCapture 
+ * @param {单个DOM节点} dom
+ * @param {事件名} eventName
+ * @param {事件方法} func
+ * @param {是否捕获} useCapture
  */
-blog.addEvent = function (dom, eventName, func, useCapture) {
-    if (window.attachEvent) {
-        dom.attachEvent("on" + eventName, func);
-    } else if (window.addEventListener) {
-        if (useCapture != undefined && useCapture === true) {
-            dom.addEventListener(eventName, func, true);
-        } else {
-            dom.addEventListener(eventName, func, false);
-        }
+blog.addEvent = function(dom, eventName, func, useCapture) {
+  if (window.attachEvent) {
+    dom.attachEvent('on' + eventName, func)
+  } else if (window.addEventListener) {
+    if (useCapture != undefined && useCapture === true) {
+      dom.addEventListener(eventName, func, true)
+    } else {
+      dom.addEventListener(eventName, func, false)
     }
+  }
 }
 
 /**
  * 工具，DOM添加某个class
- * @param {单个DOM节点} dom 
- * @param {class名} className 
+ * @param {单个DOM节点} dom
+ * @param {class名} className
  */
-blog.addClass = function (dom, className) {
-    if (!blog.hasClass(dom, className)) {
-        var c = dom.className || '';
-        dom.className = c + " " + className;
-        dom.className = blog.trim(dom.className);
-    }
+blog.addClass = function(dom, className) {
+  if (!blog.hasClass(dom, className)) {
+    var c = dom.className || ''
+    dom.className = c + ' ' + className
+    dom.className = blog.trim(dom.className)
+  }
 }
 
 /**
  * 工具，DOM是否有某个class
- * @param {单个DOM节点} dom 
- * @param {class名} className 
+ * @param {单个DOM节点} dom
+ * @param {class名} className
  */
-blog.hasClass = function (dom, className) {
-    var list = (dom.className || '').split(/\s+/);
-    for (var i = 0; i < list.length; i++) {
-        if (list[i] == className)
-            return true;
-    }
-    return false;
+blog.hasClass = function(dom, className) {
+  var list = (dom.className || '').split(/\s+/)
+  for (var i = 0; i < list.length; i++) {
+    if (list[i] == className) return true
+  }
+  return false
 }
 
 /**
  * 工具，DOM删除某个class
- * @param {单个DOM节点} dom 
- * @param {class名} className 
+ * @param {单个DOM节点} dom
+ * @param {class名} className
  */
-blog.removeClass = function (dom, className) {
-    if (blog.hasClass(dom, className)) {
-        var list = (dom.className || '').split(/\s+/);
-        var newName = '';
-        for (var i = 0; i < list.length; i++) {
-            if (list[i] != className)
-                newName = newName + ' ' + list[i];
-        }
-        dom.className = blog.trim(newName);
+blog.removeClass = function(dom, className) {
+  if (blog.hasClass(dom, className)) {
+    var list = (dom.className || '').split(/\s+/)
+    var newName = ''
+    for (var i = 0; i < list.length; i++) {
+      if (list[i] != className) newName = newName + ' ' + list[i]
     }
+    dom.className = blog.trim(newName)
+  }
 }
 
 /**
  * 工具，DOM切换某个class
- * @param {单个DOM节点} dom 
- * @param {class名} className 
+ * @param {单个DOM节点} dom
+ * @param {class名} className
  */
-blog.toggleClass = function (dom, className) {
-    if (blog.hasClass(dom, className)) {
-        blog.removeClass(dom, className);
-    } else {
-        blog.addClass(dom, className);
-    }
+blog.toggleClass = function(dom, className) {
+  if (blog.hasClass(dom, className)) {
+    blog.removeClass(dom, className)
+  } else {
+    blog.addClass(dom, className)
+  }
 }
 
 /**
  * 工具，兼容问题，某些OPPO手机不支持ES5的trim方法
- * @param {字符串} str 
+ * @param {字符串} str
  */
-blog.trim = function (str) {
-    return str.replace(/^\s+|\s+$/g, '');
+blog.trim = function(str) {
+  return str.replace(/^\s+|\s+$/g, '')
+}
+
+/**
+ * 工具，转换实体字符防止XSS
+ * @param {字符串} str
+ */
+blog.encodeHtml = function(html) {
+  var o = document.createElement('div')
+  o.innerText = html
+  var temp = o.innerHTML
+  o = null
+  return temp
 }
 
 /***********************************************************************/
@@ -115,7 +124,6 @@ blog.trim = function (str) {
 //         blog.removeClass(menu, 'show');
 //     });
 // });
-
 
 // 回到顶部
 // blog.addLoadEvent(function () {
@@ -164,6 +172,9 @@ blog.addLoadEvent(function () {
 });
 */
 //主题标识
-blog.addLoadEvent(function () {
-    console.info("Theme By %chttps://github.com/TMaize/tmaize-blog", "color: #FF0000;");
-});
+blog.addLoadEvent(function() {
+  console.info(
+    'Theme By %chttps://github.com/TMaize/tmaize-blog',
+    'color: #FF0000;'
+  )
+})
--- static/js/search.js
+++ static/js/search.js
@@ -1,70 +1,50 @@
----
-layout: null
----
-
-blog.posts = [
-{% for post in site.posts %}
-{
-"title" : "{{ post.title }}",
-"time" : "{{ post.date | date: "%Y/%m/%d" }}",
-"categories":"{% for cat in post.categories %}{{ cat }}{% if forloop.rindex != 1 %}@{% endif %}{% endfor %}",
-"url" : "{{site.baseUrl}}{{ post.url }}"
-}
-{% if forloop.rindex != 1  %}
-,
-{% endif %}
-{% endfor %}
-]
-
-
 // 搜索功能
-blog.addLoadEvent(function () {
-    var text_input = document.getElementById("search-input");
-    text_input.value='';
-    var search_list = document.getElementById("search-list");
-    var oldInput = "";
-    blog.addEvent(text_input, 'input', function () {
-        var newInput = blog.trim(text_input.value);
-        if (oldInput != blog.trim(newInput)) {
-            text_input.value = newInput;
-            oldInput = newInput;
-            search_list.innerHTML = '';
-            if (newInput != '') {
-                search(newInput);
-            }
-        }
-    });
-
-    function search(keywords) {
-        keywords = keywords.split(/\s+/);
-        for (var i = 0; i < blog.posts.length; i++) {
-            var flag = true;
-            for (var j = 0; j < keywords.length; j++) {
-                // 多个关键词取交集
-                if (!(blog.posts[i].time.match(keywords[j]) || blog.posts[i].title.toLowerCase().match(keywords[j].toLowerCase()) || blog.posts[i].categories.toLowerCase().match(keywords[j].toLowerCase()))) {
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag) {
-                addResult(blog.posts[i]);
-            }
-        }
+blog.addLoadEvent(function() {
+  // 缓存字符
+  var texts = []
+  var items = document.getElementsByClassName('search-item')
+  for (var i = 0; i < items.length; i++) {
+    texts.push(items[i].innerText.replace(/\s/g, '').toLowerCase())
+  }
+  var text_input = document.getElementById('search-input')
+  var oldInput = ''
+  blog.addEvent(text_input, 'input', function() {
+    var newInput = blog.trim(text_input.value)
+    if (oldInput != blog.trim(newInput)) {
+      oldInput = newInput
+      if (newInput == '') {
+        return
+      }
+      // 隐藏所有
+      for (var i = 0; i < texts.length; i++) {
+        setItemStatus(i, false)
+      }
+      // 重新根据关键词显示
+      search(newInput)
     }
+  })
 
-    function addResult(post) {
-        var node = document.getElementById('search-templete').children[0].cloneNode(true);
-        node.getElementsByClassName('categories')[0].innerHTML = '';
-        node.getElementsByClassName('time')[0].innerText = post.time;
-        node.getElementsByClassName('title')[0].innerText = post.title;
-        node.getElementsByClassName('title')[0].setAttribute('href', blog.contextPath + post.url);
-        var categories = post.categories.split('@');
-        for (var i = 0; i < categories.length; i++) {
-            var cat = document.getElementById('search-templete').getElementsByTagName('a')[1].cloneNode(true);
-            cat.innerText = categories[i];
-            cat.setAttribute('href', cat.getAttribute('baseUrl')+ categories[i]);
-            node.getElementsByClassName('categories')[0].appendChild(cat);
+  function search(keywords) {
+    // 大小写不敏感
+    keywords = keywords.toLowerCase().split(/\s+/)
+    for (var i = 0; i < texts.length; i++) {
+      for (var j = 0; j < keywords.length; j++) {
+        if (texts[i].indexOf(keywords[j]) != -1) {
+          setItemStatus(i, true)
+          break
         }
-        search_list.appendChild(node);
+      }
     }
-});
+  }
+  function setItemStatus(index, show) {
+    var item = document.getElementsByClassName('search-item')[index]
+    if (!item) {
+      return
+    }
+    if (show) {
+      item.style.display = 'block'
+    } else {
+      item.style.display = 'none'
+    }
+  }
+})
